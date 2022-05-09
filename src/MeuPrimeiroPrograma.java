adb2c09f7aefb1e606566512acbd0913786e8bab
Diego Celestino <diegocdsantos@gmail.com>
2022-05-09 17:31:53 -0300

    Aula sobre variáveis e tipos de variáveis. Sobre a palavra reservada var e seu uso.

---
+++ src/MeuPrimeiroPrograma.java
@@ -0,0 +1,28 @@
+public class MeuPrimeiroPrograma {
+    public static void main(String[] args){
+
+        var login = "ola";
+        var password = "uaheuhauhea";
+
+
+
+        //int = inteiros
+        //double = double
+        //bool = booleanos
+        //string = texto
+
+        String texto = "texto";
+        int inteiro = 10;
+        double percent = 23.43;
+        boolean isDriver = true;
+
+        System.out.println(texto);
+        System.out.println(inteiro);
+        System.out.println(percent);
+        System.out.println(isDriver);
+
+
+
+        System.out.println();
+    }
+}
