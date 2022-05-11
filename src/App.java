84bb76e22a488dc9a70d526aedf99f0720361a54
Diego Celestino <diegocdsantos@gmail.com>
2022-05-11 15:53:02 -0300

    Aulas sobre tipos primitivos e objetos, comparação entre int e Integer. Aula sobre escopo de variáveis.

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+
+
+
+    public static void main(String[] args){
+        // escopo
+
+        if(true){
+            int x = 10;
+        }else {
+            x = 2;
+        }
+        
+
+    }
+
+
+}
--- src/MegaSena.java
+++ src/MegaSena.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MegaSena {
 
-    public static void main(String[] args){
+    public static void MegaSena(String[] args){
 
         Scanner scanner = new Scanner(System.in);
 
