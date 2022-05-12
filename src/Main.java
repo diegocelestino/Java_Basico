c7d54cc30904c66db1327d97923f60f35af73ced
Diego Celestino <diegocdsantos@gmail.com>
2022-05-12 17:16:05 -0300

    Utilização do toString. No java é importante conhecer esses métodos que parecem um pouco contra-intuitivos, tais como o próprio .toString e o .equals

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args){
 
-        System.out.println(Fibonacci.getValue(5));
+        int[] numbers = new int[] {2, 4, 1, 6, 8, 3, 5, 5};
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+        
 
     }
 }
